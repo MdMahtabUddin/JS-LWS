2f315904a012f67233812872b3f39e97fdfefd78
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-08-24 23:18:25 +0600

    added

---
+++ for-of.js
@@ -0,0 +1,9 @@
+// LWS-37
+
+// The JavaScript for of statement loops through the values of an iterable object.
+
+// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more
+
+// for in returns index !
+// for of returns value 
+

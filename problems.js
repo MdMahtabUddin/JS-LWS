3df80d6aa909fdff1d95a70587ac51b598820fb3
Md Mahtab Uddin <72162966+MdMahtabUddin@users.noreply.github.com>
2022-09-01 07:28:20 +0000

    added

---
+++ problems.js
@@ -0,0 +1,14 @@
+// আমরা কিভাবে ১ থেকে ৬ পর্যন্ত প্রিন্ট করতে পারি । 
+
+function randomNumber(number){
+const mynumber =Math.random()* 6;
+const numbers = Math.round(mynumber) 
+    return numbers;
+     }
+    console.log(randomNumber(6));
+    
+
+    // কিভাবে আমরা সবার নাম এলফেবিটিকালি সাজাতে পারি 
+
+
+    
